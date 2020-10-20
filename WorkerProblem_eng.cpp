649dbdee32a98e7e36a2fa759ce4a605b4b9ce17
DetectiveMiawoo <51405678+DetectiveMiawoo@users.noreply.github.com>
2020-10-20 23:12:43 +0300

    Add files via upload

---
+++ WorkerProblem_eng.cpp
@@ -0,0 +1,25 @@
+// created by DetectiveMiawoo
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int worker,day;
+
+    cout << "how many workers will work on this job: ";
+    cin >> worker;
+
+    cout << " a worker how long finish this job:";
+    cin >> day;
+
+    float day2 = (float)day/(float)worker;
+
+    if (day2 < 1)
+        cout << " this job will finish in " << (float)day2 <<" hours.";
+
+    else
+        cout << " this job will finish in " << (int)day2 <<" days.";
+
+
+}
